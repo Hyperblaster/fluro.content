0a28ec78394e36c5423f87a85023d0e6a653969f
Cade <cade@hyperblaster.org>
2015-12-04 14:16:52 +1100

    Partial population

--- dist/fluro.content.js
+++ dist/fluro.content.js
@@ -310,7 +310,9 @@ angular.module('fluro.content').service('FluroContentRetrieval', function($cache
         //////////////////////////////////////
 
          //Query all of the nodes by a GET request
-        return controller.retrieveMultiple(ids, noCache, {select:fields}).then(deferred.resolve, deferred.reject);
+        controller.retrieveMultiple(ids, noCache, {select:fields}).then(deferred.resolve, deferred.reject);
+
+        return deferred.promise;
 
     }
 
